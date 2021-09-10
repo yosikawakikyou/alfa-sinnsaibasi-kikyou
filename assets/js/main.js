5509ae639e655671614f69ebb278b032e0ec24d2
朱西 <89883807+yosikawakikyou@users.noreply.github.com>
2021-09-11 00:23:10 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,15 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: '９',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'B',
           // q2: 'えええ',
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'ううう',
+          q1: '２',
           // q2: 'かかか',
           // q3: 'ききき',
         }
